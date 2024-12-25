30afdef70c631c34546c1136100d4e09ddb9c903
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2024-12-25 17:29:03 +0900

    fix: encoding@

--- splitAddress.py
+++ splitAddress.py
@@ -18,13 +18,10 @@ def get_address_by_postal_code(zipcode):
 def main():
     # CSVファイルを指定
     input_file = "address2025.csv"
-    # input_file = "test_address.csv"
     output_file = "pixus_new_receivers.csv"
-    # output_file = "test_output.csv"
     # CSVファイルを読み込む
-    # df = pd.read_csv("address2025.csv", encoding="shift-jis")
-    input_df = pd.read_csv(input_file, encoding="shift-jis")
-    output_df = pd.read_csv(output_file, encoding="shift-jis")
+    input_df = pd.read_csv(input_file)
+    output_df = pd.read_csv(output_file)
 
 
     new_data  = []
@@ -33,6 +30,7 @@ def main():
         "honor": input_df["敬称"],
         "hurigana": input_df["フリガナ"],
         "joint_name": input_df["氏名(連名１)"],
+        "joint_hurigana": input_df["フリガナ(連名1)"],
         "postal": input_df["郵便番号"],
         "address": input_df["住所1"],
         "building": input_df["住所2"],
@@ -40,12 +38,13 @@ def main():
 
     # 1.郵便番号と住所から都道府県、市区町村、それ以外を抽出し、
     # 2.CSVファイルに書き込む
-    for postal_code, address, building, name, joint_name, honor, hurigana in zip(
+    for postal_code, address, building, name, joint_name, joint_name_hurigana, honor, hurigana in zip(
         input["postal"],
         input["address"],
         input["building"],
         input["name"], 
         input["joint_name"], 
+        input["joint_hurigana"],
         input["honor"], 
         input["hurigana"]):
         # 郵便番号と住所から都道府県、市区町村、それ以外を抽出
@@ -56,11 +55,17 @@ def main():
 
         if(pd.isnull(joint_name)):
             joint_name = ""
+            joint_name_hurigana = ""
+
 
         if(pd.isnull(building)):
             building = ""
 
-        print(name, hurigana, joint_name, honor, prefecture, city, street_no, building)
+        postal_code = postal_code.replace("-", "")
+        name = name.replace("　", " ")
+        hurigana = hurigana.replace("　", " ")
+
+        print(name, hurigana, joint_name, joint_name_hurigana, honor, prefecture, city, street_no, building)
         # CSVファイルに書き込む
 
         data = {
@@ -68,11 +73,18 @@ def main():
             "フリガナ": hurigana,
             "敬称": honor,
             "氏名(連名1)": joint_name,
+            "フリガナ(連名1)" : joint_name_hurigana,
+            "敬称(連名1)" : honor,
             "郵便番号1": postal_code,
             "都道府県1": prefecture,
             "市区町村1": city,
             "地名番地1": street_no,
-            "ビル名1" : building
+            "ビル名1" : building,
+            "分類(住所)1" : "自宅",
+            "グループ" : "松治、いつよ",
+            "記録1" : "2025,0,0,0,0",
+            "記録2" : "2024,0,0,0,0",
+            "記録3" : "2023,0,0,0,0",
         }
         new_data.append(data)
 
@@ -83,7 +95,7 @@ def main():
     output_df = pd.concat([output_df, new_df], ignore_index=True)
 
     # CSVファイルに書き込む
-    output_df.to_csv(output_file, index=False, encoding="shift-jis")
+    output_df.to_csv(output_file, index=False)
 
 
 if __name__ == "__main__":
